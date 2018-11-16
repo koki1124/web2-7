88b2011855f7f3907bbae3cf37b5e6d5bd24e378
koki1124 <38421061+koki1124@users.noreply.github.com>
2018-11-16 19:37:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,5 @@
 var number=Math.floor(Math.random()*3);
-var answer=ParseInt(window.prompt('数あてゲーム。0～2の数字を入力！'));
+var answer=parseInt(window.prompt('数あてゲーム。0～2の数字を入力！'));
 
 var message;
 if(answer === number){
