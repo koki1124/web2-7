f9ab2af299e0db60a7f08d977ac3eaf70f9ddc23
koki1124 <38421061+koki1124@users.noreply.github.com>
2018-11-16 19:18:09 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,4 @@
-window.alert('こんにちは世界'); 
-console.log('こんにちは');
+var result = window.confirm('準備OK?');
 
-document.getElementById('choice').textContent =new Date();
+document.getElementById('choice').textContent =　
 
