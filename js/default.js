295aba71bf2c980f0a4ac508fce61768e01d7957
koki1124 <38421061+koki1124@users.noreply.github.com>
2018-11-20 18:33:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,7 +2,7 @@ var number=Math.floor(Math.random()*3);
 var answer=parseInt(window.prompt('数あてゲーム。0～2の数字を入力！'));
 var message;
 
-for (int i=0;i<10;i++){
+for (var i=0;i<10;i++){
 if(answer === number){
   message= 'あたり';
   break;
